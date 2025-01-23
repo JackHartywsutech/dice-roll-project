15b6315c928afe44fb79d3aa40a18b3bdeed3db8
JackHartywsutech <jharty@wsutech.edu>
2025-01-23 10:23:16 -0600

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+basic.showString("Roll!")
 
 basic.forever(function () {
+   
     input.onGesture(Gesture.Shake, function() {
         let number = randint(1, 6); 
-        basic.showNumber(number)
+        basic.showNumber(number); 
+        music.play(music.tonePlayable(Note.FSharp5, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+  
     })
 })
+
+
